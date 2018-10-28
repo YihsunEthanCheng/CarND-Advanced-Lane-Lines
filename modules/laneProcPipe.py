fd0eb93da8116bbf1312ec63215de53d21cb2170
Yihsun Ethan Cheng <33135703+YihsunEthanCheng@users.noreply.github.com>
2018-10-27 23:37:58 -0700

    Used the two stright lane images to find the desired perspective transfromation, which rsults in straigt lines in the warped image

--- modules/laneProcPipe.py
+++ modules/laneProcPipe.py
@@ -16,15 +16,17 @@ import pickle
 #%%
 class laneProcessPipeline(object):
     
+    
     def __init__(self, params):
         self.__dict__= params.copy()
         try:
-            (self.mtx, self.dist) = pickle.load(open('camera_params.pickle','rb'))
+            (self.mtx, self.dist, self.Tp) = pickle.load(open('camera_params.pickle','rb'))
         except:
             self.calib()
         img = cv2.imread(self.calibPath + '/calibration1.jpg')
         self.imgCalibOut = np.hstack((img, self.undistort(img)))
-
+        
+        
     def calib(self):
         # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
         objp = np.zeros((6*9,3), np.float32)
@@ -35,8 +37,8 @@ class laneProcessPipeline(object):
         objpoints = [] # 3d points in real world space
         imgpoints = [] # 2d points in image plane.
         for fn in fns:
-            img = cv2.imread(fn)
-            gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+            img = mpimg.imread(fn)
+            gray = cv2.cvtColor(img,cv2.COLOR_RGB2GRAY)
            
             # Find the chessboard corners
             ret, corners = cv2.findChessboardCorners(gray, (9,6),None)
@@ -48,75 +50,102 @@ class laneProcessPipeline(object):
         
                 # Draw and display the corners
                 img = cv2.drawChessboardCorners(img, (9,6), corners, ret)
-
         ret, self.mtx, self.dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
-        pickle.dump((lpp.mtx, lpp.dist),open('camera_params.pickle','wb'))
-        return self.imgCalibOut
+        self.find_perspective_transform()
+        pickle.dump((self.mtx, self.dist, self.Tp),open('camera_params.pickle','wb'))
         
-    def cut_and_show(self, gray, cut, figID):
+    def find_perspective_transform(self):
+        src = np.array([[[200, 720]], [[568, 468]], [[712, 468]], [[1080,720]]], dtype= np.float32)
+        dst = np.array([[[300, 720]], [[300, 0]], [[980, 0]], [[980,720]]], dtype= np.float32)
+        self.Tp = cv2.getPerspectiveTransform(src, dst)        
+       
+    def warp_perspective(self, img, figID = 0):
+        wimg = cv2.warpPerspective(img, self.Tp, img.shape[:2][::-1], flags=cv2.INTER_LINEAR)
+        if figID > 0:
+            wimg[:,290:300,0] = 255
+            wimg[:,980:990,0] = 255
+            fig = plt.figure(figID)
+            fig.clf()
+            axes = fig.subplots(1,2, sharey = True)
+            axes[0].imshow(img)
+            axes[0].set_title('original')
+            axes[1].imshow(wimg)
+            axes[1].set_title('Warped')
+            fig.subplots_adjust(left = 1/16., right = 1.0 - 1./16,
+                top = 1- 1./32, bottom = 1./32 , wspace=1./32 )
+        return wimg
+    
+    
+    def cut_and_show(self, gray, cut, figID, title):
         mask = np.zeros_like(gray)
         mask[(gray >= cut[0]) & (gray <= cut[1])] = 1
         if figID > 0:
             plt.figure(figID)
             plt.imshow(mask*255, cmap = 'gray')
+            plt.title(title)
         return mask   
             
-    def undistort(self, img):
-        return cv2.undistort(img, self.mtx, self.dist, None, self.mtx)
+    
+    def undistort(self, img, figID = 0):
+        undist = cv2.undistort(img, self.mtx, self.dist, None, self.mtx)
+        if figID > 0:
+            plt.figure(figID)
+            plt.imshow(np.hstack((img, undist)))
+            plt.title('Input image   vs.   undistorted image')
+        return undist
         
-    def process(self, img):
-        return img
     
     def imread(self, fname):
-        return cv2.imread(self.imgPath + '/' + fname)
+        return mpimg.imread(self.imgPath + '/' + fname)
         
-    def select_color(self, img, figID = 0):
-        hls = cv2.cvtColor(img, cv2.COLOR_BGR2HLS)[:,:,self.HLS_select];
-#        color_mask = np.zeros_like(gray)
-#        color_mask[(gray > self.HLS_cut[0]) & (gray <= self.HLS_cut[1])] = 1
-        return self.cut_and_show(hls, self.HLS_cut, figID)
+    
+    def select_color(self, img, figID = 0, title = ''):
+        hls = cv2.cvtColor(img, cv2.COLOR_RGB2HLS)[:,:,self.HLS_select];
+        return self.cut_and_show(hls, self.HLS_cut, figID, title)
         
-    def extract_gradeient(self, img, figID = 0):
-        img = mpimg.imread(self.imgPath + '/' + fname)
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    
+    def extract_gradient(self, img, figID = 0, title = ''):
+        gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
         gradx = cv2.Sobel(gray, cv2.CV_64F, 1, 0)
         abs_sobelx = np.absolute(gradx)
         scaled_sobel = np.uint8(255*abs_sobelx/np.max(abs_sobelx))
-#        sxbinary = np.zeros_like(scaled_sobel)
-#        sxbinary[(scaled_sobel >= gradx_cut[0]) & (scaled_sobel <= thresh_max)] = 1
-        self.cut_and_show(scaled_sobel, self.gradx_cut, figID)
+        return self.cut_and_show(scaled_sobel, self.gradx_cut, figID, title)
+        
+    
+    def process(self, img, show = False):
+        undist = self.undistort(img, int(show)*1)
+        color_mask = self.select_color(undist, int(show)*2, 'color mask')
+        grad_mask = self.extract_gradient(undist, int(show)*3, 'x-gradient mask')
+        stack_mask = np.dstack((np.zeros_like(color_mask), grad_mask, color_mask))*255
+        if show:
+            plt.figure(int(show)*4)
+            plt.imshow(stack_mask)
+            plt.title('Combined color + gradient mask')         
+            
+        return img
+    
 
-    
-    #%%
-    def extract_edge():
-        
-    def apply_ROI()    
- 
-        
-    def select_color():
-        
-    def select_gradient():
-         
-    
-#%%
+
 params = {
         'calibPath' : 'camera_cal',
         'imgPath'   : 'test_images',
         'HLS_select' : 2, # pick saturation
         'HLS_cut' : [90, 255],
-        'gradx_cut': [20, 100]
+        'gradx_cut': [20, 100],
+        'rows': 720,
+        'cols': 1280,       
         } 
         
 lpp = laneProcessPipeline(params)
+lpp.calib()
 #imshow(self.imgCalibOut)
-
 #%%
-img = lpp.imread('test1.jpg')
-lpp.select_color(img, 2)
 
-
-
-
-
-
+lpp.warp_perspective(lpp.undistort(lpp.imread('test1.jpg')),1)
+lpp.warp_perspective(lpp.undistort(lpp.imread('test2.jpg')),2)
+lpp.warp_perspective(lpp.undistort(lpp.imread('test3.jpg')),3)
+lpp.warp_perspective(lpp.undistort(lpp.imread('test4.jpg')),4)
+lpp.warp_perspective(lpp.undistort(lpp.imread('test5.jpg')),5)
+lpp.warp_perspective(lpp.undistort(lpp.imread('straight_lines1.jpg')),6)
+lpp.warp_perspective(lpp.undistort(lpp.imread('straight_lines2.jpg')),7)
 
