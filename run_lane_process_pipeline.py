5cf5db5cf49b26ed79ee4ed5a074f9c1f6a2fa77
Yihsun Ethan Cheng <33135703+YihsunEthanCheng@users.noreply.github.com>
2018-10-28 19:14:47 -0700

    Added metric per pixel in x/y to compute curavture and center deviation in meter

--- run_lane_process_pipeline.py
+++ run_lane_process_pipeline.py
@@ -13,16 +13,19 @@ from modules.laneProcPipe import laneFinder
 params = {
         'calibPath' : 'camera_cal',
         'imgPath'   : 'test_images',
-        'outPath' : 'output_images',
+        'outPath'   : 'output_images',
         'HLS_select' : 2, # pick saturation
-        'HLS_cut' : [90, 255],
+        'HLS_cut'  : [90, 255],
         'gradx_cut': [20, 100],
         'rows': 720,
         'cols': 1280,
         # peak search params       
-        'nwindows' : 9,
+        'nwindows': 9,
         'margin': 100, # width of the windows
         'minpix': 50
+        # Define conversions in x and y from pixels space to meters
+        'ym_per_pix': 30/720  # meters per pixel in y dimension
+        'xm_per_pix': 3.7/700 # meters per pixel in x dimension
         } 
         
 lane_detect = laneFinder(params)
@@ -30,5 +33,7 @@ lane_detect = laneFinder(params)
 #lpp.unit_test()
 img = lane_detect.imread('test1.jpg')
 binary_warped = lane_detect.process(img)
-left_fit, right_fit = lane_detect.find_polyfit_lanes(binary_warped, 3)
+left_fit, right_fit = lane_detect.find_polylanes(binary_warped, 3)
+left_fitx, right_fitx, left_fit_refined, right_fit_refined = lane_detect.refine_polylanes(binary_warped, left_fit, right_fit, 4)
+
 self = lane_detect
